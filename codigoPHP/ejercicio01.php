e2cef7148e9eb4bea2a376d986c1d2bd90496740
SrPozuelo <oscar.pozvil@educa.jcyl.es>
2026-03-03 12:19:35 +0100

    Update Ejercicios 1,2 y 3

--- codigoPHP/ejercicio01.php
+++ codigoPHP/ejercicio01.php
@@ -23,8 +23,53 @@
                 </div>
             </div>
         </header>
-        <main>
+        <main id="Ejercicio1">
             <div id="titulo">1-Inicializar variables de distintos tipos de datos básicos (string, int, float, bool) y mostrarlos (echo, print, printf, print_r, var_dump).</div>
+            <?php
+                /**
+                * @author: Óscar Pozuelo Villamandos
+                * @since: 03/03/2026
+                * 1-Inicializar variables de los distintos tipos de datos básicos(string, int, float, bool) y mostrar los datos por pantalla (echo, print, printf, print_r,var_dump).
+                */
+                $sCadena="¡Hola mundo!";
+                $iEntero=1425;
+                $fDecimal=98.15;
+                $bBooleano=false;
+                $aDatos=[$sCadena,$iEntero,$fDecimal,$bBooleano];
+                echo("<h3>Usando echo</h3>");
+                echo('<p>La variable $sCadena es de tipo <span class="tipo">"'.gettype($sCadena).'"</span> y contiene el valor <span class="valor">'.$sCadena.'</span></p><br>');
+                echo('<p>La variable $iEntero es de tipo <span class="tipo">"'.gettype($iEntero).'"</span> y contiene el valor <span class="valor">'.$iEntero.'</span></p><br>');
+                echo('<p>La variable $fDecimal es de tipo <span class="tipo">"'.gettype($fDecimal).'"</span> y contiene el valor <span class="valor">'.$fDecimal.'</span></p><br>');
+                echo('<p>La variable $bBooleano es de tipo <span class="tipo">"'.gettype($bBooleano).'"</span> y contiene el valor <span class="valor">'.$bBooleano.'</span></p><br>');
+
+                print("<h3>Usando print</h3>");
+                print('La variable $scadena es de tipo <span class="tipo">"'.gettype($sCadena).'"</span> y contiene el valor <span class="valor">'.$sCadena.'</span><br>');
+                print('La variable $ientero es de tipo <span class="tipo">"'.gettype($iEntero).'"</span> y contiene el valor <span class="valor">'.$iEntero.'</span><br>');
+                print('La variable $fdecimal es de tipo <span class="tipo">"'.gettype($fDecimal).'"</span> y contiene el valor <span class="valor">'.$fDecimal.'</span><br>');
+                print('La variable $bBooleano es de tipo <span class="tipo">"'.gettype($bBooleano).'"</span> y contiene el valor <span class="valor">'.($bBooleano?"true":"false").'</span><br>');
+
+                printf("<h3>Usando printf</h3>");
+                printf("La variable %s es de tipo <span class='tipo'>%s</span> y tiene el valor <span class='valor'>%s</span> <br>", '$texto', gettype($sCadena), $sCadena);
+                printf("La variable %s es de tipo <span class='tipo'>%s</span> y tiene el valor <span class='valor'>%d</span><br>", '$entero', gettype($iEntero), $iEntero);
+                printf("La variable %s es de tipo <span class='tipo'>%s</span> y tiene el valor <span class='valor'>%.2f</span> <br>", '$decimal', gettype($fDecimal), $fDecimal);
+                printf("La variable %s es de tipo <span class='tipo'>%s</span> y tiene el valor <span class='valor'>%s</span> <br>", '$booleano', gettype($bBooleano), ($bBooleano?"true":"false"));
+
+                print_r("<h3>Usando print_r</h3>");
+                print_r($aDatos,false);
+                echo("<br>");
+                print_r($aDatos,true);
+                echo("<br>");
+                print_r('La variable $scadena es de tipo <span class="tipo">'.gettype($sCadena).'</span> y tiene el valor <span class="valor">'.$sCadena.'</span><br>');
+                print_r('La variable $ientero es de tipo <span class="tipo">'.gettype($iEntero).'</span> y tiene el valor <span class="valor">'.$iEntero.'</span><br>');
+                print_r('La variable $fdecimal es de tipo <span class="tipo">'.gettype($fDecimal).'</span> y tiene el valor <span class="valor">'.$fDecimal.'</span><br>');
+                print_r('La variable $booleano es de tipo <span class="tipo">'.gettype($bBooleano).'</span> y tiene el valor <span class="valor">'.($bBooleano?"true":"false").'</span><br>');
+
+                echo("<h3>Usando var_dump</h3>");
+                var_dump($sCadena);echo("<br>");
+                var_dump($iEntero);echo("<br>");
+                var_dump($fDecimal);echo("<br>");
+                var_dump($bBooleano);echo("<br>");
+            ?>
         </main>
         <footer class="pie-pagina">
             <div class="contenido-footer">
