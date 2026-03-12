a3130992680fdf90d97947a4dcaa4a0f1a84bb58
SrPozuelo <oscar.pozvil@educa.jcyl.es>
2026-03-12 13:55:48 +0100

    update ejercicio 24.

--- codigoPHP/ejercicio24.php
+++ codigoPHP/ejercicio24.php
@@ -38,7 +38,6 @@
                 require_once "../core/080126libreriaValidacion.php";
                 //Variable interruptor que nos indica que la entrada es correcta
                 $entradaOK=true;
-                $oFecha=new DateTime();
                 //Array asociativo preparado para recoger los mensajes de error
                 $aErrores=[
                     'CodDepartamento'          =>'', 
@@ -63,7 +62,7 @@
                     $aErrores['DescDepartamento']=validacionFormularios::comprobarAlfaNumerico($_REQUEST['DescDepartamento'],1000,1,1);
                     $aErrores['FechaCreacionDepartamento']=validacionFormularios::validarFecha($_REQUEST['FechaCreacionDepartamento']);
                     $aErrores['VolumenDeNegocio']= validacionFormularios::comprobarFloatMonetarioES($_REQUEST['VolumenDeNegocio'],PHP_FLOAT_MAX,0,1);
-                    $aErrores['FechaBajaDepartamento']=validacionFormularios::validarFecha($_REQUEST['FechaBajaDepartamento'],"31-12-9999",$oFecha->format("d-m-Y"),0);
+                    $aErrores['FechaBajaDepartamento']=validacionFormularios::validarFecha($_REQUEST['FechaBajaDepartamento'],"31-12-9999",(new DateTime())->format("d-m-Y"),0);
                     foreach($aErrores as $campo => $valor){
                         if(!empty($valor)){
                             // Comprobar si el valor es válido
@@ -76,19 +75,26 @@
                     $entradaOK = false;
                 }
                 //Tratamiento del formulario
-                if($entradaOK){ //Cargar la variable $aRespuestas y tratamiento de datos OK
+                if($entradaOK){
+                    //Cargar la variable $aRespuestas y tratamiento de datos OK
                     // Recuperar los valores del formulario
+                    date_default_timezone_set("Europe/Madrid");
+                    setlocale(LC_TIME,'es_ES.UTF-8','es_ES','spanish');
                     $aRespuestas['CodDepartamento']=$_REQUEST['CodDepartamento'];
                     $aRespuestas['DescDepartamento']=$_REQUEST['DescDepartamento'];
-                    $oFecha=new DateTime($_REQUEST['FechaCreacionDepartamento']);
-                    $aRespuestas['FechaCreacionDepartamento']=$oFecha->format("d-m-Y");
+                    $aRespuestas['FechaCreacionDepartamento']=(new DateTime($_REQUEST['FechaCreacionDepartamento']));
                     $aRespuestas['VolumenDeNegocio']=$_REQUEST['VolumenDeNegocio'].' €';
-                    $oFecha=new DateTime($_REQUEST['FechaBajaDepartamento']);
-                    (empty($_REQUEST['FechaBajaDepartamento']))?$aRespuestas['FechaBajaDepartamento']='No tiene':$aRespuestas['FechaBajaDepartamento']=$oFecha->format("d-m-Y");
+                    (empty($_REQUEST['FechaBajaDepartamento']))?$aRespuestas['FechaBajaDepartamento']='No tiene':$aRespuestas['FechaBajaDepartamento']=(new DateTime($_REQUEST['FechaBajaDepartamento']));
                     echo "<h3>Respuestas del formulario:</h3>";
-                    foreach ($aRespuestas as $campo => $valor) {
-                        echo "<p>".$campo."=".$valor."</p>";
+                    foreach ($aRespuestas as $campo => $valor){
+                        if($valor instanceof DateTime){
+                            echo "<p>".$campo."=".strftime("%d-%m-%Y",$valor->getTimestamp())."</p>";
+                        }
+                        else{
+                            echo "<p>".$campo."=".$valor."</p>";
+                        }
                     }
+                    echo("<p>".$_REQUEST['FechaCreacionDepartamento']."</p>");
                 }
                 else {
                     //Mostrar el formulario hasta que lo rellenemos correctamente
@@ -102,7 +108,7 @@
                                         <label for="cod">Código:</label>
                                     </td>
                                     <td>
-                                        <input type="text" name="CodDepartamento" class="texto obligatorio" id="cod" value="<?php echo(isset($_REQUEST["CodDepartamento"])&&empty($aErrores["CodDepartamento"]))?$_REQUEST["CodDepartamento"]:''?>">
+                                        <input type="text" name="CodDepartamento" class="texto obligatorio" id="CodDepartamento" value="<?php echo(isset($_REQUEST["CodDepartamento"])&&empty($aErrores["CodDepartamento"]))?$_REQUEST["CodDepartamento"]:''?>">
                                     </td>
                                     <td class="span">
                                         <span><?php echo $aErrores['CodDepartamento']?></span>
@@ -113,7 +119,7 @@
                                         <label for="desc">Descripción:</label>
                                     </td>
                                     <td>
-                                        <input type="text" name="DescDepartamento" class="texto obligatorio" id="desc" value="<?php echo(isset($_REQUEST["DescDepartamento"])&&empty($aErrores["DescDepartamento"]))?$_REQUEST["DescDepartamento"]:''?>">
+                                        <input type="text" name="DescDepartamento" class="texto obligatorio" id="DescDepartamento" value="<?php echo(isset($_REQUEST["DescDepartamento"])&&empty($aErrores["DescDepartamento"]))?$_REQUEST["DescDepartamento"]:''?>">
                                     </td>
                                     <td class="span">
                                         <span><?php echo $aErrores['DescDepartamento']?></span>
@@ -124,7 +130,7 @@
                                         <label for="crea">Fecha de creación:</label>
                                     </td>
                                     <td>
-                                        <input type="text" name="FechaCreacionDepartamento" class="fecha bloqueado" id="crea" value="<?php $oFecha=new DateTime(); echo $oFecha->format('d').'/'.$oFecha->format('m').'/'.$oFecha->format('Y')?>" readonly>
+                                        <input type="text" name="FechaCreacionDepartamento" class="fecha bloqueado" id="FechaCreacionDepartamento" value="<?php echo(new DateTime())->format('d-m-Y');?>" readonly>
                                     </td>
                                     <td class="span">
                                         <span><?php echo $aErrores['FechaCreacionDepartamento']?></span>
@@ -135,7 +141,7 @@
                                         <label for="Vol">Volumen de negocio:</label>
                                     </td>
                                     <td>
-                                        <input type="text" name="VolumenDeNegocio" class="texto obligatorio" id="crea" value="<?php echo(isset($_REQUEST["VolumenDeNegocio"])&&empty($aErrores["VolumenDeNegocio"]))?$_REQUEST["VolumenDeNegocio"]:''?>">
+                                        <input type="text" name="VolumenDeNegocio" class="texto obligatorio" id="VolumenDeNegocio" value="<?php echo(isset($_REQUEST["VolumenDeNegocio"])&&empty($aErrores["VolumenDeNegocio"]))?$_REQUEST["VolumenDeNegocio"]:''?>">
                                     </td>
                                     <td class="span">
                                         <span><?php echo $aErrores['VolumenDeNegocio']?></span>
@@ -146,7 +152,7 @@
                                         <label for="baja">Fecha de baja:</label>
                                     </td>
                                     <td>
-                                        <input type="date" name="FechaBajaDepartamento" class="fecha" id="date" value="<?php echo(isset($_REQUEST["FechaBajaDepartamento"])&&empty($aErrores["FechaBajaDepartamento"]))?$_REQUEST["FechaBajaDepartamento"]:''?>">
+                                        <input type="date" name="FechaBajaDepartamento" class="fecha" id="FechaBajaDepartamento" value="<?php echo(isset($_REQUEST["FechaBajaDepartamento"])&&empty($aErrores["FechaBajaDepartamento"]))?$_REQUEST["FechaBajaDepartamento"]:''?>">
                                     </td>
                                     <td class="span">
                                         <span><?php echo $aErrores['FechaBajaDepartamento']?></span>
